e69c3a28819b99b2f9ef1dd56bfec4329335a173
shirdel <sara.shirdel72@yahoo.com>
2017-11-06 15:56:47 +0330

    add method to Person List() Profile() Register() Edit() LoginUpdate()

---
+++ Retailpayment/src/retailpayment/Person.java
@@ -0,0 +1,82 @@
+package retailpayment;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Person class is abstract
+ * 
+ * @author z.shirdel
+ *
+ */
+public class Person {
+	int userId;
+	String nickName;
+	String firstName;
+	String lastName;
+	Date lastLoginOn;
+	int applicationId;
+	String status;
+	String registerOn;
+	int nationalCode;
+	int accountNo;
+	String IBAN;
+	String description;
+
+	enum Role {
+		Merchant, Promoter, Buyer
+	};
+
+	public List list() {
+		return null;
+
+	}
+
+	/**
+	 * show your profile
+	 * 
+	 * @return
+	 */
+	public String Profile() {
+		return null;
+
+	}
+
+	/**
+	 * register and set your data
+	 * 
+	 * @return
+	 */
+	public boolean Register(int userId, String nickName, String firstName, String lastName, Date lastLoginOn,
+			int applicationId, String status, String registerOn, int nationalCode, int accountNo, String IBAN,
+			String description) {
+		
+		return false;
+
+	}
+
+	/**
+	 * edit your profile
+	 * 
+	 * @return
+	 */
+	public boolean Edit(int userId, String nickName, String firstName, String lastName, Date lastLoginOn,
+			int applicationId, String status, String registerOn, int nationalCode, int accountNo, String IBAN,
+			String description) {
+		
+		return false;
+
+	}
+
+	/**
+	 * last log in in app
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public boolean LoginUpdate(Date date) {
+		return false;
+
+	}
+
+}
