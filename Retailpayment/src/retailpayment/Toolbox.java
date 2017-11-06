5596ea20a9f1cc93146ebe89c0cd28a472d10916
shirdel <sara.shirdel72@yahoo.com>
2017-11-06 13:00:14 +0330

    add methods for convert jalalli date to Gregorian date convertToGregorian(Date) convertToGregorian(String) getGregorianCalendar(int,int,int)

--- Retailpayment/src/retailpayment/Toolbox.java
+++ Retailpayment/src/retailpayment/Toolbox.java
@@ -1,32 +1,139 @@
 package retailpayment;
 
 import com.google.gson.Gson;
-import java.io.UnsupportedEncodingException;    
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.io.UnsupportedEncodingException;
 import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
+/**
+ * Toolbox class provides the tools
+ * 
+ * @author z.shirdel
+ *
+ */
 public class Toolbox {
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public static int g_days_in_month[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	public static int j_days_in_month[] = { 31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29 };
 
-	
-	
-	public String toJason(Object obj) {
+	/**convert object to Json */
+	public String toJson(Object obj) {
 		Gson gson = new Gson();
-
 		// convert java object to JSON format
 		String json = gson.toJson(obj);
 		return json;
+	}
+
+	/**
+	 * convertToGregorian method get Jalalli date object and return Gregorian
+	 * date string;
+	 */
+	public String convertToGregorian(Date date) {
+
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH);
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		return getGregorianCalendar(year, month, day);
+
+	}
+
+	/**
+	 * convertToGregorian method get Jalalli date string and return Gregorian
+	 * date string;
+	 */
+	public String convertToGregorian(String dateString) {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		Date date = null;
+		try {
+			date = dateFormat.parse(dateString);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return convertToGregorian(date);
+	}
+
+	/** This function performs the conversion of Jalali to Gregorian; */
+	public String getGregorianCalendar(int year, int month, int day) {
+		String date_gregorian = null;
+		int gy, gm, gd;
+		int jy, jm, jd;
+		long g_day_no, j_day_no;
+		boolean leap;
+
+		int i;
+
+		jy = year - 979;
+		jm = month - 1;
+		jd = day - 1;
+
+		j_day_no = 365 * jy + (jy / 33) * 8 + (jy % 33 + 3) / 4;
+		for (i = 0; i < jm; ++i)
+			j_day_no += j_days_in_month[i];
+
+		j_day_no += jd;
+
+		g_day_no = j_day_no + 79;
+
+		gy = (int) (1600 + 400 * (g_day_no
+				/ 146097)); /*
+							 * 146097 = 365*400 + 400/4 - 400/100 + 400/400
+							 */
+		g_day_no = g_day_no % 146097;
+
+		leap = true;
+		if (g_day_no >= 36525) /* 36525 = 365*100 + 100/4 */
+		{
+			g_day_no--;
+			gy += 100 * (g_day_no
+					/ 36524); /* 36524 = 365*100 + 100/4 - 100/100 */
+			g_day_no = g_day_no % 36524;
+
+			if (g_day_no >= 365)
+				g_day_no++;
+			else
+				leap = false;
+		}
+
+		gy += 4 * (g_day_no / 1461); /* 1461 = 365*4 + 4/4 */
+		g_day_no %= 1461;
+
+		if (g_day_no >= 366) {
+			leap = false;
+
+			g_day_no--;
+			gy += g_day_no / 365;
+			g_day_no = g_day_no % 365;
+		}
+
+		for (i = 0; g_day_no >= g_days_in_month[i] + parsBooleanToInt(i == 1 && leap); i++)
+			g_day_no -= g_days_in_month[i] + parsBooleanToInt(i == 1 && leap);
+
+		gm = i + 1;
+		gd = (int) (g_day_no + 1);
+
+		Date date = new GregorianCalendar(gy, gm, gd).getTime();
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+		date_gregorian = dateFormat.format(date);
+		return date_gregorian;
+	}
+
+	private static int parsBooleanToInt(Boolean sample) {
+		if (sample)
+			return 1;
+		else
+			return 0;
 	}
 
 }
