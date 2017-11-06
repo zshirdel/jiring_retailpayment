255057d254e56f46639ff50381450522874aaea6
zshirdel <sara.shirdel93@gmail.com>
2017-11-06 16:13:16 +0330

    test

---
+++ Retailpayment/src/retailpayment/mainclass.java
@@ -0,0 +1,10 @@
+package retailpayment;
+
+public class mainclass {
+	public static void main(String[] args) {
+		Toolbox tool=new Toolbox();
+	//	Date date=new
+	System.out.println(tool.convertToGregorian("1396/8/15"));
+	}
+
+}
