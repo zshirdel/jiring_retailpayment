4ede53d00580aef9310e8ce2ef3ba6ed1b3c5926
zshirdel <sara.shirdel93@gmail.com>
2017-11-07 13:39:56 +0330

    add methods in transaction Pay() Turnover() Brief() Clear() Archive() ConfirmGen()

--- src/retailpayment/Transaction.java
+++ src/retailpayment/Transaction.java
@@ -1,5 +1,93 @@
 package retailpayment;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * pay money
+ * 
+ * @author z.shirdel
+ *
+ */
 public class Transaction {
+	int TransactionId;
+	Date RegisterOn;
+	int BuyerId;
+	int MerchantID;
+	float Amount;
+	int MSISDN;
+	int ConfirmCode;
+
+	enum PaymentType {
+	};
+
+	enum PaymentInterface {
+	};
+
+	/**
+	 * Pay method change money in databases
+	 * 
+	 * @param BuyerId
+	 * @param MerchantID
+	 * @param Amount
+	 * @return
+	 */
+	public boolean Pay(int BuyerId, int MerchantID, float Amount) {
+		return false;
+
+	}
+
+	/**
+	 * get last report for
+	 * 
+	 * @return
+	 */
+	public String Brief(int MSISDN) {
+		return null;
+
+	}
+
+	/**
+	 * clear reports
+	 * 
+	 * @return
+	 */
+	public boolean Clear() {
+		return false;
+
+	}
+
+	public boolean Archive() {
+		return false;
+
+	}
+
+	/**
+	 * get reports from date1 till date2
+	 * 
+	 * @param from
+	 * @param till
+	 * @return
+	 */
+	public List<String> Turnover(Date from, Date till) {
+		return null;
+
+	}
+
+	/**
+	 * get random code
+	 * 
+	 * @return
+	 */
+	public int ConfirmGen() {
+		Random rand = new Random();
+		// rand.nextInt((max - min) + 1) + min;
+		int random = rand.nextInt((999999 - 100000) + 1) + 100000;
+		ConfirmCode = random;
+
+		return random;
+
+	}
 
 }
--- src/retailpayment/mainclass.java
+++ src/retailpayment/mainclass.java
@@ -4,7 +4,9 @@ public class mainclass {
 	public static void main(String[] args) {
 		Toolbox tool=new Toolbox();
 	//	Date date=new
-	System.out.println(tool.convertToGregorian("1396/8/15"));
+		Transaction trans =new Transaction();
+	//System.out.println(tool.convertToGregorian("1396/8/15"));
+		System.out.println(trans.ConfirmGen()+"ssssssss"+trans.ConfirmCode);
 	}
 
 }
